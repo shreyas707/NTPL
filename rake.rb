dc0c62d712ed0656981aac2e09cf87a69c3cc83e
shreyas <hello@shreyas.im>
2018-03-06 20:42:52 +0530

    enhancements in rake file

--- rake.rb
+++ rake.rb
@@ -43,42 +43,57 @@ customer_names.each_with_index do |customer_name, index|
 	c.save
 end
 
-sizes = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
-15.times do
-	product = Product.new
-	product.size = sizes.sample
-	product.category_id = Category.all.pluck(:id).sample
-	product.save
+all_sizes = [1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 6.5, 7, 7.5, 8, 8.5, 9, 9.5, 10]
+i = 1
+5.times do
+	sizes = all_sizes.sample(3).sort
+	j = 0
+	3.times do
+		product = Product.new
+		product.size = sizes[j]
+		product.category_id = Category.find(i).id
+		product.save
+		j += 1
+	end
+	i += 1
 end
 
-30.times do 
-	s = Sauda.new
-	if Sauda.exists?
-		sauda = Sauda.order(:id).last
-		s.number = "NTPL - #{sauda.id + 1}"
-	else
-		s.number = "NTPL - 1"
+6.times do
+	j = 0
+	customers = Customer.all.pluck(:id)
+	5.times do
+		s = Sauda.new
+		if Sauda.exists?
+			sauda = Sauda.order(:id).last
+			s.number = "NTPL - #{sauda.id + 1}"
+		else
+			s.number = "NTPL - 1"
+		end
+		s.date = Date.today
+		s.customer_id = customers[j]
+		s.is_order_taken = false
+		s.is_delivered = false
+		s.save!
+		j += 1
 	end
-	s.date = Date.today
-	s.customer_id = Customer.all.pluck(:id).sample
-	s.is_order_taken = false
-	s.is_delivered = false
-	s.save!
 end
 
 i = 1
 numbers = [100, 200, 300, 400, 500]
 quantities = [10, 20, 30, 40, 50]
-120.times do
-	slt = SaudaLineItem.new
-	slt.category_id = Category.all.pluck(:id).sample
-	slt.rate = numbers.sample
-	slt.quantity = quantities.sample
-	if i == 30
-		i = 1
+30.times do
+	j = 0
+	categories = Category.all.pluck(:id).sample(2)
+	2.times do
+		slt = SaudaLineItem.new
+		slt.category_id = categories[j]
+		slt.rate = numbers.sample
+		slt.quantity = quantities.sample
+		slt.sauda_id = i
+		slt.save!
+		j += 1
 	end
-	slt.sauda_id = i
-	slt.save!
+	i += 1
 end
 
 
